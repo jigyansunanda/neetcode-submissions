cf398ac019bac7468b92be4136d128603ead7a57
Jigyansu Nanda <62047889+jigyansunanda@users.noreply.github.com>
2026-06-25 13:35:09 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.go
@@ -0,0 +1,14 @@
+func search(nums []int, target int) int {
+	lo, hi := -1, len(nums)
+	for hi > lo + 1 {
+		mid := lo + (hi - lo)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			lo = mid
+		} else {
+			hi = mid
+		}
+	}
+	return -1
+}
